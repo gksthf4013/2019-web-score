534456b4842ee6d757c964a8cc3cc1f9aec40e38
gksthf4013 <gksthf4013@naver.com>
2019-08-12 13:55:19 +0900

    중간 완성

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+var scoreURL = {
+	site: "https://webmir.co.kr/score",
+	cURL: site + "/score_in.php",
+	rURL: site + "/score_li.php",
+	uURL: site + "/score_up.php",
+	dURL: site + "/score_del.php"
+}
