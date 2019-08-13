124303b8ee114de7bc0a50967c6cec91b7b76ded
gksthf4013 <gksthf4013@naver.com>
2019-08-13 15:21:03 +0900

    pagerMaker 완성

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,10 @@
+// 삼항연산자
+// 조건?참일때:거짓일때
+console.log(3 > 5 ? "참" : "거짓");
+
+
+
+
 var html = '';
 var site = "https://webmir.co.kr/score";
 var scoreURL = {
@@ -6,9 +13,14 @@ var scoreURL = {
 	uURL: site + "/score_up.php",
 	dURL: site + "/score_del.php"
 }
+var nowPage = 1;
 
-getList(4);
+getList(nowPage);
+// 리스트 가져오기 
+// get / https://webmir.co.kr/score.score_li.php / page
 function getList(page) {
+	// nowPage를 page에 넣음
+	nowPage = page; //지역변수 page의 값을 전역변수 nowPage에 넣는다
 	$.ajax({
 		type: "get",
 		url: scoreURL.rURL,
@@ -18,70 +30,154 @@ function getList(page) {
 		dataType: "json",
 		success: function (res) {
 			console.log(res);
-			for(var i in res.student) {
-				html  = '<tr>';
-				html += '<td>'+res.student[i].stdname+'</td>';
-				html += '<td>'+res.student[i].kor+'</td>';
-				html += '<td>'+res.student[i].eng+'</td>';
-				html += '<td>'+res.student[i].math+'</td>';
+			$(".score-tb").find("tbody").empty();
+			for (var i in res.student) {
+				html = '<tr>';
+				html += '<td>' + res.student[i].stdname + '</td>';
+				html += '<td>' + res.student[i].kor + '</td>';
+				html += '<td>' + res.student[i].eng + '</td>';
+				html += '<td>' + res.student[i].math + '</td>';
 				html += '<td class="text-center">';
-				html += '<button class="btn btn-success">수정</button> ';
-				html += '<button class="btn btn-danger">삭제</button>';
+				html += '<button class="btn btn-success bt-up" data-id="' + res.student[i].id + '">수정</button> ';
+				html += '<button class="btn btn-danger bt-del" data-id="' + res.student[i].id + '">삭제</button>';
 				html += '</td>';
 				html += '</tr>';
 				$(".score-tb").find("tbody").append(html);
 			}
 			pagerMaker(res.total, page);
+			deletMaker();
 		},
-		error: function(xhr) {
+		error: function (xhr) {
 			alert("통신이 실패했습니다 관리자에게 문의하세요.");
-		  console.log(xhr);
+			console.log(xhr);
 		}
 	});
 }
 
+
+// 페이저 생성
 function pagerMaker(total, page) {
-	var div = 3;                   // 세트당 나올 페이지 수
-	var cnt = Math.ceil(total/10);  // 전체 페이지 개수
-	var stn = 0;                    // 세트중에 시작페이지
-	var edn = 0;                    // 세트중에 마지막페이지
-	var prev = 0;                   // < 를 클릭시 나타날 페이지
-	var next = 0;                   // > 를 클릭시 나타날 페이지
+	var div = 3; // 세트당 나올 페이지 수
+	var cnt = Math.ceil(total / 10); // 전체 페이지 개수
+	var stn = 0; // 세트중에 시작페이지
+	var edn = 0; // 세트중에 마지막페이지
+	var prev = 0; // < 를 클릭시 나타날 페이지
+	var next = 0; // > 를 클릭시 나타날 페이지
+	var prevShow = false;
+	var lastShow = false;
 	var first = 1;
 	var last = cnt;
-	var lastIndex = (Math.ceil(cnt/div) - 1);   // 페이지 세트의 마지막 index
-	var nowIndex = (Math.ceil(page/div - 1));   // 현재 페이지 세트의 index
+	var lastIndex = (Math.ceil(cnt / div) - 1); // 페이지 세트의 마지막 index
+	var nowIndex = (Math.ceil(page / div - 1)); // 현재 페이지 세트의 index
 
-	stn = nowIndex * div + 1;                  // 세트의 시작페이지 값
+	stn = nowIndex * div + 1; // 세트의 시작페이지 값
 	if (cnt < stn + div - 1) edn = cnt;
-	else edn = stn + div -1;
+	else edn = stn + div - 1;
 
-	console.log("stn:"+stn);
-	console.log("edn:"+edn);
-	console.log("lastIndex:"+lastIndex);
-	console.log("nowIndex:"+nowIndex);
+	if (nowIndex > 0) {
+		prevShow = true;
+		prev = stn - 1;
+	}
+
+	if (lastIndex > nowIndex) {
+		lastShow = true;
+		next = edn + 1;
+	}
+
+	console.log("stn:" + stn);
+	console.log("edn:" + edn);
+	console.log("lastIndex:" + lastIndex);
+	console.log("nowIndex:" + nowIndex);
 
 
-
-  html  = '<li class="page-item page-first">';
-  html += '<span class="page-link"><i class="fas fa-angle-double-left"></i></span>';
+	html = '<li class="page-item page-first ' + (prevShow ? "" : "disabled") + '" data-page="' + first + '">';
+	html += '<span class="page-link"><i class="fas fa-angle-double-left"></i></span>';
 	html += '</li>';
-  html += '<li class="page-item page-prev">';
-  html += '<span class="page-link"><i class="fas fa-angle-left"></i></span>';
+	html += '<li class="page-item page-prev ' + (prevShow ? "" : "disabled") + '" data-page="' + prev + '">';
+	html += '<span class="page-link"><i class="fas fa-angle-left"></i></span>';
 	html += '</li>';
-	for(var i=1; i<=cnt; i++) {
-		html += '<li class="page-item page-ct">';
-		html += '<span class="page-link">'+i+'</span>';
+	for (var i = stn; i <= edn; i++) {
+		html += '<li class="page-item page-ct ' + (page == i ? "active" : "") + '" data-page="' + i + '">';
+		html += '<span class="page-link">' + i + '</span>';
 		html += '</li>';
 	}
-  html += '<li class="page-item page-next">';
-  html += '<span class="page-link"><i class="fas fa-angle-right"></i></span>';
+	html += '<li class="page-item page-next ' + (lastShow ? "" : "disabled") + '" data-page="' + next + '">';
+	html += '<span class="page-link"><i class="fas fa-angle-right"></i></span>';
 	html += '</li>';
-  html += '<li class="page-item page-last">';
-  html += '<span class="page-link"><i class="fas fa-angle-double-right"></i></span>';
+	html += '<li class="page-item page-last ' + (lastShow ? "" : "disabled") + '" data-page="' + last + '">';
+	html += '<span class="page-link"><i class="fas fa-angle-double-right"></i></span>';
 	html += '</li>';
 	$(".pager").html(html);
-	if(page == 1) $(".page-lt").addClass("disabled");
-	if(page == cnt) $(".page-rt").addClass("disabled");
-	$(".page-ct").eq(page-1).addClass("active");
+	$(".page-item").click(function () {
+		if (!$(this).hasClass("disabled")) getList($(this).data("page"));
+		// !(not): true일 경우엔 false로, false일 경우 true로 바꿔준다
+	});
 }
+
+
+// 리스트 삭제하기
+function deletMaker() {
+	// post / https://webmir.co.kr/score.score_del.php / id
+	$(".bt-del").click(function () {
+		if (confirm("정말로 삭제하시겠습니까?")) {
+			$.ajax({
+				type: "post",
+				url: scoreURL.dURL,
+				data: {
+					id: $(this).data("id")
+				},
+				dataType: "json",
+				success: function (res) {
+					if (res.code == 200) getList(nowPage);
+					else alert("데이터 처리가 실패했습니다. 관리자에게 문의하세요.");
+				}
+			});
+		};
+	});
+}
+
+// 데이터 저장
+$("#bt-save").click(function(){
+	var stdname = $.trim($("#stdname").val());
+	var kor = Number($("#kor").val());  // Number(); => 문자열을 숫자열로 바꿔줌
+	var eng = Number($("#eng").val());
+	var math = Number($("#math").val());
+	if(kor == 0) kor = "0";
+	if(eng == 0) eng = "0";
+	if(math == 0) math = "0";
+	if(stdname == "") {
+		alert("학생 이름을 입력해 주세요.");
+		$("#stdname").focus();
+		return;
+	}
+	if(kor == "" || kor<0 || kor>100) {  // || = or 과 같은 뜻
+		alert("올바른 국어점수를 입력하세요.");
+		$("#kor").focus();
+		return;
+	}
+	if(eng == "" || eng<0 || eng>100) {  // || = or 과 같은 뜻
+		alert("올바른 영어점수를 입력하세요.");
+		$("#eng").focus();
+		return;
+	}
+	if(math == "" || math<0 || math>100) {  // || = or 과 같은 뜻
+		alert("올바른 수학점수를 입력하세요.");
+		$("#math").focus();
+		return;
+	}
+	$.ajax({
+		type: "post",
+		url: scoreURL.cURL,
+		data: {
+			stdname: stdname,
+			kor: kor,
+			eng: eng,
+			math: math
+		},
+		dataType: "json",
+		success: function (res) {
+			if (res.code == 200) getList(nowPage);
+			else alert("데이터 처리가 실패했습니다. 관리자에게 문의하세요.");
+		}
+	});
+});
